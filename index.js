d2872e62b30782456831173848a3d96e1cbf5c0b
Bartosz Sienkiewicz <b.sienkiew@gmail.com>
2026-01-08 13:56:28 +0100

    change startup port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = 3030;
 
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
